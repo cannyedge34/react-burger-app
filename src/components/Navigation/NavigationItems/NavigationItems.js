f572a1fa20ebd398d0864ce3ade552a5beb260d2
1paciente <1paciente.com@gmail.com>
2018-01-23 13:21:20 +0100

    added some testing examples

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,7 @@ import NavigationItem from './NavigationItem/NavigationItem';
 
 const navigationItems = props => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem exact link="/">
+    <NavigationItem link="/" exact>
       Burger Builder
     </NavigationItem>
     {props.isAuthenticated ? (
